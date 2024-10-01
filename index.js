f71eed3480c4c3698c3cb4ada06a86fe13a2e3fa
Erik Demchak <kushikime@MacBook-Pro-Erik.local>
2024-10-01 21:48:49 +0200

    add dotenv config

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Server init');
+require('dotenv').config();
+const http = require('http');
+
+http.createServer().listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
