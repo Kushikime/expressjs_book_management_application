4ebc62acd301e5310055c622eccacf5655e9e7d2
Erik Demchak <kushikime@MacBook-Pro-Erik.local>
2024-10-02 22:03:59 +0200

    following the autor dont question the stupid code blocks xD

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,10 +1,45 @@
+require('dotenv').config();
+
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 
+const { findUser, saveUser } = require('../database/db');
+
+const UserModel = require('../models/userModel');
+const mongoose = require('mongoose');
+
 router.post('/register', (req, res) => {
-  // find user
-  // if user -> User exists.
-  // if !user -> encryt passowrd, save user record, return accessToken
+  findUser({
+    email: req.body.email,
+  })
+    .then((user) => {
+      if (user) {
+        return res.status(409).json({
+          message: 'User exist',
+        });
+      }
+
+      const emptyUser = new UserModel();
+      emptyUser._id = new mongoose.Types.ObjectId();
+      const newUser = Object.assign(emptyUser, req.body);
+
+      bcrypt.hash(req.body.password, 10, async (err, hash) => {
+        if (err) {
+          return res.status(501).json({ message: 'Error: ' + err.messageF });
+        }
+
+        newUser.password = hash;
+        const userRecord = await saveUser(newUser);
+        res.status(201).json({ message: 'Successful', ...userRecord });
+      });
+    })
+    .catch((err) => ({
+      error: {
+        message: err.message,
+        status: err.status,
+      },
+    }));
 });
 
 router.post('login', (req, res) => {});
