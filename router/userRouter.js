cda0b50c3e7ad03cf5a9410b72840ac9161faed9
Erik Demchak <kushikime@MacBook-Pro-Erik.local>
2024-10-02 22:16:34 +0200

    refactor a bit register, intall jsonwebtoken

--- router/userRouter.js
+++ router/userRouter.js
@@ -9,37 +9,45 @@ const { findUser, saveUser } = require('../database/db');
 const UserModel = require('../models/userModel');
 const mongoose = require('mongoose');
 
-router.post('/register', (req, res) => {
-  findUser({
-    email: req.body.email,
-  })
-    .then((user) => {
-      if (user) {
-        return res.status(409).json({
-          message: 'User exist',
-        });
+const handleError = (res, status, message) => {
+  return res.status(status).json({ message });
+};
+
+const hashPassword = (password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(hash);
       }
+    });
+  });
+};
 
-      const emptyUser = new UserModel();
-      emptyUser._id = new mongoose.Types.ObjectId();
-      const newUser = Object.assign(emptyUser, req.body);
+router.post('/register', async (req, res) => {
+  try {
+    const user = await findUser({ email: req.body.email });
+    if (user) {
+      return handleError(res, 409, 'User exists');
+    }
 
-      bcrypt.hash(req.body.password, 10, async (err, hash) => {
-        if (err) {
-          return res.status(501).json({ message: 'Error: ' + err.messageF });
-        }
+    const emptyUser = new UserModel({});
+    emptyUser._id = new mongoose.Types.ObjectId();
+    const newUser = Object.assign(emptyUser, req.body);
 
-        newUser.password = hash;
-        const userRecord = await saveUser(newUser);
-        res.status(201).json({ message: 'Successful', ...userRecord });
-      });
-    })
-    .catch((err) => ({
-      error: {
-        message: err.message,
-        status: err.status,
-      },
-    }));
+    try {
+      newUser.password = await hashPassword(req.body.password);
+      const userRecord = await saveUser(newUser);
+      res
+        .status(201)
+        .json({ message: 'User successfully registered', ...userRecord });
+    } catch (err) {
+      return handleError(res, 501, 'Error: ' + err.message);
+    }
+  } catch (err) {
+    return handleError(res, 500, 'Error: ' + err.message);
+  }
 });
 
 router.post('login', (req, res) => {});
