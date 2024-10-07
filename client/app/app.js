0efd0d7d946a6918898b96bcd7519d431ee1c9ac
Erik Demchak <kushikime@MacBook-Pro-Erik.local>
2024-10-08 01:44:32 +0200

    add base ejs markup for pages

---
+++ client/app/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const router = require('../routes/router');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
+
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+app.use('/', router);
+
+module.exports = app;
